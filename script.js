c45aade8b7278f3cb1e1efae8f61ed2e118b3dea
Haidar-yousif <haidaryousif40@gmail.com>
2024-12-14 21:36:52 +0200

    testing 404.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//update path based on the language
 document.addEventListener("DOMContentLoaded", () => {
     const langSwitchLinks = document.querySelectorAll(".lang-switch a");
 
@@ -28,4 +29,124 @@ document.addEventListener("DOMContentLoaded", () => {
             window.location.href = `/${currentPage}`;
         }
     }
+});
+//handling the burgle click
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const navLinks = document.querySelector('.nav');
+    const page = document.querySelector('body'); // Target the whole body or specific content sections
+
+    burger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+        burger.classList.toggle('open');
+        page.classList.toggle('hide-body-content');
+    });
+});
+// header scroll animation
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector(".header");
+     
+    window.addEventListener("scroll", () => {
+     
+        if (window.scrollY > 50) {  // If scrolled down more than 50px
+            header.classList.add("shrink");
+        } else {
+            header.classList.remove("shrink");
+        }
+    });
+});
+//handling missing contact form input 
+document.addEventListener("DOMContentLoaded",()=>{
+const nameInput=document.getElementById("nom");
+const emailInput =document.getElementById("destinataire");
+const MessageInput=document.querySelector("textarea");
+const SubjectInput=document.getElementById("sujet");
+const nameError=document.querySelector(" .error-nom");
+const emailError=document.querySelector(".error-destin");
+
+let isvalidNom=false;
+let isvalidMail=false;
+
+// Initialize EmailJS
+emailjs.init('kiiyjRptDBbgH5nwW');
+nameInput.addEventListener("blur",()=>{
+if(nameInput.value.trim()===""){
+isvalidNom=false;
+    nameError.style.display ="block";
+}
+else {
+    isvalidNom=true;
+    nameError.style.display ="none";
+}
+});
+
+emailInput.addEventListener("blur", () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (emailInput.value.trim() === "") {
+        isvalidMail=false
+        emailError.style.display = "block";
+    } else if (!emailRegex.test(emailInput.value)) {
+    isvalidMail=false
+        emailError.style.display = "block";
+    } else {
+        isvalidMail=true;
+        emailError.style.display = "none";
+    }
+});
+document.getElementById("send_mail").addEventListener("click", (e) => {
+    e.preventDefault();
+    const preferredLang = localStorage.getItem("preferredLang");
+
+    const Pobup={
+        "en":{
+            "success":{
+                title: "Success!",
+                text: "Your message has been sent.",
+                icon: "success",
+                confirmButtonText: "OK",
+            },
+            "error":{
+                title: "Error!",
+                text: "There was an issue sending your message.",
+                icon: "error",
+                confirmButtonText: "Try Again",
+            }
+        },
+        "fr":{
+            "success": {
+                title: "Succès!",
+                text: "Votre message a été envoyé.",
+                icon: "success",
+                confirmButtonText: "D'accord",
+            },
+            "error": {
+                title: "Erreur!",
+                text: "Il y a eu un problème lors de l'envoi de votre message.",
+                icon: "error",
+                confirmButtonText: "Réessayer",
+            }
+        }
+    
+    }
+  
+    if (isvalidNom && isvalidMail) {
+        const param={
+            from_name:nameInput.value,
+            email_id:emailInput.value,
+            subject:SubjectInput.value,
+            message:MessageInput.value
+        }
+        // Send email using EmailJS
+        emailjs.send("service_6g2zndm", "template_g3m6fsk",
+            param
+        ).then(() => {
+            Swal.fire(Pobup[preferredLang]["success"]);
+        }).catch((error) => {
+            Swal.fire(
+                Pobup[preferredLang]["error"]
+            );
+            console.error("EmailJS Error:", error);
+        });
+    }
+});
 });
